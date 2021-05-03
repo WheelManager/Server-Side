79f6ae8965951b562fb1b9ff3e540742e3e48938
Asvife <victorfernando.67@gmail.com>
2021-05-03 03:13:04 -0500

    create feature-brand
    create feature brand

---
+++ src/main/java/com/hackdead/wheelmanager/repository/IBrandRepository.java
@@ -0,0 +1,13 @@
+package com.hackdead.wheelmanager.repository;
+
+import com.hackdead.wheelmanager.entities.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface IBrandRepository extends JpaRepository<Brand,Long> {
+    public List<Brand> findByName(String name);
+}
