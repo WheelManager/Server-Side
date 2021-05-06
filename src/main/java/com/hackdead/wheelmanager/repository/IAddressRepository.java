1d099c14a9ca9688b0b62ffd0cfba50e5a95c33b
Hugo Arauco <araparhug7@gmail.com>
2021-05-06 17:55:50 -0500

    create feature-address

---
+++ src/main/java/com/hackdead/wheelmanager/repository/IAddressRepository.java
@@ -0,0 +1,12 @@
+package com.hackdead.wheelmanager.repository;
+
+import com.hackdead.wheelmanager.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IAddressRepository extends JpaRepository<Address,Long> {
+    public List<Address> findByLongitudeAndLatitude(Double longitude, Double latitude);
+}
